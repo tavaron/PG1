2b0ea1d033b2236f7c5d0805da0bafc08f8bbc5d
Marius Christ <marius.christ@gmail.com>
2011-10-10 02:17:08 +0200

    UE02 created

---
+++ UE02/main.c
@@ -0,0 +1,26 @@
+/*
+ *      ------------------------------------------------------------------------
+ *      
+ *      Datei:      UE02/main.c
+ *      
+ *      Name:       Marius Christ
+ *      Matr.-Nr.:  954402
+ *      Datum:      10.10.2011 v0.0
+ *      geaendert:  10.10.2011
+ *      
+ *      Beschreibung:   !!!
+ *      
+ *      Eingabe:    !!!
+ *      
+ *      Ausgabe:    !!!
+ *      
+ *      ------------------------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
