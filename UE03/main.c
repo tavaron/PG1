ba98251b650ca8096c6ca4ba33aaf050707b4cb0
Marius Christ <marius@ares.(none)>
2011-10-13 14:11:59 +0200

    UE03 fertig

--- UE03/main.c
+++ UE03/main.c
@@ -18,17 +18,33 @@
 
 #include <stdio.h>
 
+void spaces(int count);
+void numbers(int count);
+
 int main(int argc, char** argv)
 {
-	
+	int i = 0;
+	for(i=0;i<=9;i++)
+	{
+		spaces(9-i);
+		numbers(i);
+		printf("\n");
+	}
 	return 0;
 }
 
 void spaces(int count)
 {
+	int i = 0;
+	for(i=0;i<count;i++)
+		printf(" ");
 }
 
 void numbers(int count)
 {
-	
+	int i = 0;
+	for(i=count;i>=0;i--)
+		printf("%d",i);
+	for(i=1;i<=count;i++)
+		printf("%d",i);
 }
