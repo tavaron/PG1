c1ae0361810fb889f6c7165b04d7d473d8c15d4c
Marius Christ <marius@ares.(none)>
2011-10-13 11:09:24 +0200

    Vorlesung 20111013, UE03 erstellt

---
+++ UE03/main.c
@@ -0,0 +1,34 @@
+//      ------------------------------------------------------------------------
+//      
+//      Datei: main.c
+//      
+//      Name:		Marius Christ
+//      Matr.-Nr.:	954402
+//      Datum:		13.10.2011 v0.1
+//      geaendert:	13.10.2011
+//      
+//      Beschreibung: !!!
+//      
+//      Eingabe: !!!
+//      
+//      Ausgabe: !!!
+//      
+//      ------------------------------------------------------------------------
+
+
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	
+	return 0;
+}
+
+void spaces(int count)
+{
+}
+
+void numbers(int count)
+{
+	
+}
