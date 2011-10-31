4cb1054de1bd74072c5b512808051853e26f91fe
Marius Christ <marius@ares.(none)>
2011-10-31 17:01:11 +0100

    UE05 fertig

--- UE05/neu.c
+++ UE05/neu.c
@@ -26,21 +26,34 @@ int getDigitCount(char* string);
 
 int main(int argc, char** argv)
 {
+	while(1){
 	char mainString[stringLength];
 	char subString[100];
 	
 	printf("Bitte Zeichenkette eingeben: ");
 	scanf("%10s", mainString);
-	if(checkLength(mainString)==stringLength) scanf("%s", subString);
+	if(checkLength(mainString)==4&&mainString[0]=='e'&&mainString[1]=='x'&&mainString[2]=='i'&&mainString[3]=='t')
+	{
+		printf("Exit...");
+		break;
+	}
+	
+	subString[0] = '\0';
+	if(checkLength(mainString)==stringLength&&checkLength(subString)==0) scanf("%s", subString);
 	
 	printf("MainString: %s\n", mainString);
 	printf("Anzahl Zeichen MainString: %d\n", checkLength(mainString));
-	if(checkLength(mainString)==stringLength) printf("SubString: %s\n", subString);
-	else printf("No SubString!\n");
+	printf("Anzahl Zeichen SubString: %d\n", checkLength(subString));
+	
+	if(checkLength(mainString)==stringLength&&checkLength(subString)==0) printf("SubString: %s\n", subString);
+	else if(checkLength(subString)==0) printf("No SubString!\n");
+	
 	if(checkLength(mainString)==stringLength) printf("Die Zeichenfolge enthaelt %d Ziffern, %d Grossbuchstaben und %d Kleinbuchstaben.\n", getDigitCount(mainString)+getDigitCount(subString), getBigLetterCount(mainString)+getBigLetterCount(subString), getSmallLetterCount(mainString)+getSmallLetterCount(subString)); 
 	else printf("Die Zeichenfolge enthaelt %d Ziffern, %d Grossbuchstaben und %d Kleinbuchstaben.\n", getDigitCount(mainString), getBigLetterCount(mainString), getSmallLetterCount(mainString));
-	if(checkLength(mainString)==stringLength) printf("Die Zeichenfolge ist %d Zeichen lang", checkLength(mainString)+checkLength(subString));
-	else printf("Die Zeichenfolge ist %d Zeichen lang", checkLength(mainString));
+	
+	if(checkLength(mainString)==stringLength) printf("Die Zeichenfolge ist %d Zeichen lang\n", checkLength(mainString)+checkLength(subString));
+	else printf("Die Zeichenfolge ist %d Zeichen lang\n", checkLength(mainString));
+}
 	return 0;
 }
 
---
+++ UE06/main.c
@@ -0,0 +1,34 @@
+//      ------------------------------------------------------------------------
+//      
+//      Datei: main.c
+//      
+//      Name:		Marius Christ
+//      Matr.-Nr.:	954402
+//      Datum:		31.10.2011 v0.1
+//      geaendert:	31.10.2011
+//      
+//      Beschreibung: !!!
+//      
+//      Eingabe: !!!
+//      
+//      Ausgabe: !!!
+//      
+//      ------------------------------------------------------------------------
+
+
+#include <stdio.h>
+#include <math.h>
+#define genauigkeit 6
+
+int main(int argc, char** argv)
+{
+	double wert, *psin;
+	psin = &wert;
+	
+	return 0;
+}
+
+double rekSin(int x, int lauf, double *psin)
+{
+	return 0.0;
+}
