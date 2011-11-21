d8f84fe699f1f341f8f92d78a4f03bb0922f43cf
Marius Christ <marius@ares.(none)>
2011-11-21 17:14:22 +0100

    UE10 fertig

--- UE09/main.c
+++ UE09/main.c
@@ -39,10 +39,14 @@ int anzahlEintraege();
 
 int main(int argc, char** argv)
 {
+    // bis manueller Schleifenabbruch
     while(1) {
+		
         short correct = 0;
         char menu;
         char c;
+        
+        // Hauptmenü ausgeben
         printf("Bitte wählen Sie:\n\n");
         printf("1 - neuen Teilnehmer anlegen\n");
         printf("2 - Telefonliste ausgeben\n");
@@ -50,6 +54,7 @@ int main(int argc, char** argv)
         printf("4 - Programm beenden\n\n");
         printf("Auswahl: ");
         
+        // Puffer leeren und Eingabe prüfen
         while((c=getchar())!=10) {
             if(c>=49&&c<=52) {
                 menu = c;
@@ -59,6 +64,7 @@ int main(int argc, char** argv)
         
         printf("\n");
         
+        // Wenn eine korrekte Eingabe gemacht wurde, rufe passende Funktion auf
         if(correct) {
             if(menu == '1')
                 eingabe();
@@ -76,25 +82,23 @@ int main(int argc, char** argv)
 
 void eingabe()
 {
-    while(1) {
-        int i = anzahlEintraege();
-        if(anzahlEintraege()==MAX_EINTRAEGE - 1) {
-            printf("Die Liste ist bereits voll!\n\n");
-            break;
-        }
-        else {
-            printf("Sie erstellen nun Eintrag Nr. %d\n", i+1);
-            printf("Bitte geben Sie den Namen ein: ");
-            scanf("%s", liste[i].name);
-            printf("Bitte geben Sie den Vornamen ein: ");
-            scanf("%s", liste[i].vorname);
-            printf("Bitte geben Sie die Nummer ein: ");
-            scanf("%s", liste[i].nummer);
-            printf("Der Eintrag wurde erstellt...\n\n");
-            break;
-        }
-        
-    }
+	int i = anzahlEintraege();
+	// Wenn Liste voll
+	if(i==MAX_EINTRAEGE - 1)
+		printf("Die Liste ist bereits voll!\n\n");
+	// sonst
+	else {
+		// Eingabeaufforderung und Einlesen
+		printf("Sie erstellen nun Eintrag Nr. %d\n", i+1);
+		printf("Bitte geben Sie den Namen ein: ");
+		scanf("%s", liste[i].name);
+		printf("Bitte geben Sie den Vornamen ein: ");
+		scanf("%s", liste[i].vorname);
+		printf("Bitte geben Sie die Nummer ein: ");
+		scanf("%s", liste[i].nummer);
+		printf("Der Eintrag wurde erstellt...\n\n");
+	}
+    // Eingabepuffer leeren
     while(getchar()!=10);
 }
 
@@ -104,13 +108,18 @@ void ausgabe()
         k = 0;
     ausgabeHeader();
 
+	// für jeden Eintrag
     for(j=0;j<anzahlEintraege();j++) {
+		
+		// Positionsnummer ausgeben
         if(j>=99)
             printf("%d ",j+1);
         else if(j>=9)
             printf("%d  ",j+1);
         else
             printf("%d   ",j+1);
+            
+        // Ausgabe der Eintraege
         for(k=0;liste[j].name[k]!='\0';k++)
             putchar(liste[j].name[k]);
         spaces(NAME_LENGTH-k+1);
@@ -128,12 +137,19 @@ void loeschen()
 {
     int index = 0,
         i = 0;
+    
+    // Eingabeauforderung und Einlesen
     printf("Bitte geben Sie die Nummer des Eintrags an: ");
     scanf("%d", &index);
     i = index - 1;
+    
+    // Eingabepuffer leeren
     while(getchar()!=10);
+    
+    // Einträge aufrücken und dabei zu löschenden Eintrag überschreiben
     for(;i<=anzahlEintraege();i++)
         liste[i]=liste[i+1];
+    
     printf("Der Eintrag Nr. %d wurde gelöscht...\n\n", index);
 }
 
@@ -157,6 +173,7 @@ void spaces(int anzahl)
 int anzahlEintraege()
 {
     int i = 0;
+    // zähle hoch, bis Name leer ist
     for(i=0;i<MAX_EINTRAEGE;i++)
             if(liste[i].name[0]=='\0')
                 break;
--- UE10/main.c
+++ UE10/main.c
@@ -16,6 +16,7 @@
 //      ------------------------------------------------------------------------
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define NAME_LENGTH 40
 #define VORNAME_LENGTH 20
@@ -25,10 +26,203 @@ struct eintrag {
     char name[NAME_LENGTH];
     char vorname[VORNAME_LENGTH];
     char nummer[NUMMER_LENGTH];
+    // Zeiger auf den nächsten struct
+    struct eintrag *next;
 };
+// Listenkopf und -ende
+struct eintrag *anfang, *ende;
+
+// Prototypen
+void eingabe();
+void ausgabe();
+void loeschen();
+void init();
+void spaces(int anzahl);
+void ausgabeHeader();
 
 int main(int argc, char** argv)
 {
-    
+	// initialisiere Liste
+	init();
+	
+	// bis manueller Schleifenabbruch
+    while(1) {
+		
+        short correct = 0;
+        char menu;
+        char c;
+        
+        // Hauptmenü ausgeben
+        printf("Bitte wählen Sie:\n\n");
+        printf("1 - neuen Teilnehmer anlegen\n");
+        printf("2 - Telefonliste ausgeben\n");
+        printf("3 - Teilnehmer löschen\n");
+        printf("4 - Programm beenden\n\n");
+        printf("Auswahl: ");
+        
+        // Puffer leeren und Eingabe prüfen
+        while((c=getchar())!=10) {
+            if(c>=49&&c<=52) {
+                menu = c;
+                correct = 1;
+            }
+        }
+        
+        printf("\n");
+        
+        // Wenn eine korrekte Eingabe gemacht wurde, rufe passende Funktion auf
+        if(correct) {
+            if(menu == '1')
+                eingabe();
+            else if(menu == '2')
+                ausgabe();
+            else if(menu == '3')
+                loeschen();
+            else if(menu == '4')
+                break;
+        }
+    }
+    printf("Exit...\n");
     return 0;
+}
+
+void init()
+{
+	// reserviere Speicher
+	anfang = (struct eintrag*) malloc(sizeof(struct eintrag));
+	ende = (struct eintrag*) malloc(sizeof(struct eintrag));
+	
+	// Wenn Speicher nicht reserviert werden konnte
+	if(anfang == NULL || ende == NULL)
+	{
+		// werfe Fehler und beende mit Errorcode 1
+		printf("Speicherplatzmangel...\n");
+		exit(1);
+	}
+	// verkette Anfang mit Ende und baue Schleife am Ende
+	anfang->next = ende->next = ende;
+}
+
+void eingabe()
+{
+	struct eintrag *element, *tmp;
+	// Speicher für neues Element reservieren
+	element = (struct eintrag*) malloc(sizeof(struct eintrag));
+	
+	// Wenn Speicher nicht reserviert werden konnte
+	if(element == NULL)
+	{
+		// werfe Fehler und beende mit Errorcode 1
+		printf("Speicherplatzmangel...\n");
+		exit(1);
+	}
+	
+	// Eingabeaufforderung und Einlesen
+	printf("Neuer Eintrag wird angelegt...\n");
+	printf("Name: ");
+	scanf("%s", element->name);
+	printf("Vorname: ");
+	scanf("%s", element->vorname);
+	printf("Nummer: ");
+	scanf("%s", element->nummer);
+	printf("\n");
+	// Eingabepuffer leeren
+	while(getchar()!=10);
+	
+	// Erstes Element zwischenspeichern
+	tmp = anfang->next;
+	// Neues Element am Anfang einfügen
+	anfang->next = element;
+	// zwischengespeichertes Element mit neuem Element verketten
+	element->next = tmp;
+}
+
+void ausgabe()
+{
+	int i = 1,k;
+	struct eintrag *_eintrag;
+	// _eintrag mit erstem Element initialisieren
+	_eintrag = anfang->next;
+	
+	ausgabeHeader();
+	
+	// solange ende nicht erreicht
+	while(_eintrag != ende)
+	{
+		// Positionsnummer ausgeben
+		if(i>=100)
+            printf("%d ",i);
+        else if(i>=10)
+            printf("%d  ",i);
+        else
+            printf("%d   ",i);
+            
+        // Ausgabe der Eintraege
+        for(k=0;_eintrag->name[k]!='\0';k++)
+            putchar(_eintrag->name[k]);
+        spaces(NAME_LENGTH-k+1);
+        for(k=0;_eintrag->vorname[k]!='\0';k++)
+            putchar(_eintrag->vorname[k]);
+        spaces(VORNAME_LENGTH-k+1);
+        for(k=0;_eintrag->nummer[k]!='\0';k++)
+            putchar(_eintrag->nummer[k]);
+        printf("\n");
+        
+        // zum nächsten Eintrag wechseln
+		_eintrag = _eintrag->next;
+		
+		// Positionsnummer hochzählen
+		i++;
+	}
+	printf("\n");
+}
+
+void ausgabeHeader()
+{
+    printf("Nr. ");
+    printf("Name");
+    spaces(NAME_LENGTH - 3);
+    printf("Vorname");
+    spaces(VORNAME_LENGTH - 6);
+    printf("Nummer\n");
+}
+
+void spaces(int anzahl)
+{
+    int i = 0;
+    for(;i<anzahl;i++)
+        putchar(' ');
+}
+
+void loeschen()
+{
+	int index, i = 1;
+	
+	// temporären Zeiger anlegen
+	struct eintrag *tmp;
+	
+	// tmp-Zeiger auf Anfang der Liste setzen
+	tmp = anfang;
+	
+	// Nr. abfragen
+	printf("Nr. des zu löschenden Eintrags: ");
+	scanf("%d", &index);
+	printf("\n");
+	// Eingabepuffer leeren
+	while(getchar()!=10);
+	
+	// Liste durchlaufen bis zum angegebenen Eintrag
+	for(;i<=index;i++)
+	{
+		// beim erreichen des Eintrags
+		if(i==index)
+		{
+			// Speicher freigeben
+			free(tmp->next);
+			// Verkettung anpassen
+			tmp->next = tmp->next->next;
+		}
+		// Einen Eintrag weitergehen
+		tmp = tmp->next;
+	}
 }
